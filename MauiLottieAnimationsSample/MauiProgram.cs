b0873e8dd601f6e3006ae55b0f2fe48e7f71411f
Gerald Versluis <gerald@verslu.is>
2022-07-24 14:26:15 +0200

    Implement Lottie

--- MauiLottieAnimationsSample/MauiProgram.cs
+++ MauiLottieAnimationsSample/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiLottieAnimationsSample;
+﻿using SkiaSharp.Views.Maui.Controls.Hosting;
+
+namespace MauiLottieAnimationsSample;
 
 public static class MauiProgram
 {
@@ -7,6 +9,7 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			.UseSkiaSharp()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
